922532136b9d6208ae40174023e1a1a2d508a2f9
Sharbatanu Chatterjee <sharbatanu444@gmail.com>
2017-01-26 12:11:46 +0100

    first complete code without run

--- starter.py
+++ starter.py
@@ -17,7 +17,7 @@ class DummyAgent(): #name to be changed later
 
         self.parameter1 = parameter1
 
-    def visualize_trial(self, n_steps=50):
+    def visualize_trial(self, n_steps=200,lambda=0.95,tau=0.05,eta=0.01):
         """Do a trial without learning, with display.
 
         Parameters
@@ -33,20 +33,18 @@ class DummyAgent(): #name to be changed later
         plb.show()
         plb.pause(1e-7)
             
-        # make sure the mountain-car is reset
+        max_episodes = 100
+        # make sure the mountain-car is reset - 1 agent (have to do 10 times)
         self.mountain_car.reset()
 
-
-        for n in range(n_steps):
-            print('\rt =', self.mountain_car.t)
+        # below - episodes for 1 agent
+        for n in range(max_episodes):
+            #print('\rt =', self.mountain_car.t)
+            print('\repisode =', n)
             sys.stdout.flush()
             
-            ## we learnaction
-            learn()
-
-
-            # simulate the timestep
-            #self.mountain_car.simulate_timesteps(100, 0.01)
+            ## we learn action
+            learn(n_steps,lambda,tau,eta)
 
             # update the visualization
             mv.update_figure()
@@ -63,10 +61,10 @@ class DummyAgent(): #name to be changed later
         plb.show()
         plb.waitforbuttonpress(timeout=3)
 
-    def softmax_policy(self,Q, temperature):
-        Pa1 = np.exp(calculate_Q((mc.x,mc.x_d),0)/temperature)
-        Pa2 = np.exp(calculate_Q((mc.x,mc.x_d),1)/temperature)
-        Pa3 = np.exp(calculate_Q((mc.x,mc.x_d),2)/temperature)
+    def softmax_policy(self,tau):
+        Pa1 = np.exp(calculate_Q((mc.x,mc.x_d),0)/tau)
+        Pa2 = np.exp(calculate_Q((mc.x,mc.x_d),1)/tau)
+        Pa3 = np.exp(calculate_Q((mc.x,mc.x_d),2)/tau)
         P_tot = Pa1 + Pa2 + Pa3
         return [Pa1/P_tot,Pa2/P_tot,Pa3/P_tot]
 
@@ -74,7 +72,8 @@ class DummyAgent(): #name to be changed later
         sum = 0
         for keys,weight in w.items():
             if keys[2]==a:
-                sum+=weight*calculate_r(keys,(x,xd))
+                sum+=weight*calculate_r(keys,(x,xd),sigmax,sigmay)
+
         return sum
 
     def calculate_r((xj,xdj,a),(x,xd),sigmax,sigmay):
@@ -82,13 +81,12 @@ class DummyAgent(): #name to be changed later
 
 
 
-    def learn(self):
-        # This is your job! 
+    def learn(self, n_steps=200,lambda=0.95,tau=0.05,eta=0.01):
         #We will look to implement the Sarsa(lambda) algorithm pseudocode Fig 7.11
 
         #input layer (place fields)
-        x_size = 20
-        xdot_size = 20
+        x_size = 10
+        xdot_size = 10
         sigmax = 180/x_size
         sigmay = 30/xdot_size
         # input_layer = np.zeros((x_size,xdot_size))
@@ -97,7 +95,7 @@ class DummyAgent(): #name to be changed later
         a_size = 3
         output_layer = np.zeros(a_size)
 
-        #weight matrix
+        #weight dictionary matrix
         w = {}
         w_ini = 0
         for a in np.arange(0,a_size,1):
@@ -107,17 +105,17 @@ class DummyAgent(): #name to be changed later
 
         #dictionary for storing Q-values
         Q = {} 
-        for i in np.arange(0,x_size,1):
-            for j in np.arange(0,xdot_size,1):
-                for k in np.arange(0,a_size,1):
-                    Q[(i,j,k)] = 0
-                    
+        for x in np.arange(0,x_size,1):
+            for x_d in np.arange(0,xdot_size,1):
+                for a in np.arange(0,a_size,1):
+                    Q[(x,x_d,a)] = 0
+
         #dictionary for storing e-values
         e = {} 
-        for i in np.arange(0,x_size,1):
-            for j in np.arange(0,xdot_size,1):
-                for k in np.arange(0,a_size,1):
-                    e[(i,j,k)] = 0
+        for x in np.arange(0,x_size,1):
+            for x_d in np.arange(0,xdot_size,1):
+                for a in np.arange(0,a_size,1):
+                    e[(x,x_d,a)] = 0
 
 
 
@@ -131,27 +129,20 @@ class DummyAgent(): #name to be changed later
             mc.simulate_timesteps(100,0.01) #take action a, observe
 
             #choose new action according to softmax policy
-            p = softmax_policy()
+            p = softmax_policy(tau)
             a_selected = np.random.choice([0,1,2],1,p)[0]
 
             delta = mc.R + gamma*Q[(mc.x,mc.x_d,a_selected)] - Q[(x_old,x_d_old,a)]
-            e[(x_old,x_d_old,a)] += 1
+            
 
             for keys,_ in Q.items():
                 w[keys] += eta*delta*e[keys]
                 e[keys] = gamma*delta*e[keys]
+
+            e[(x_old,x_d_old,a_old)] += 1
             x_old = mc.x
             x_d_old = mc.x_d
             a_old = a_selected
-
-
-
-
-
-
-
-
-
 
 if __name__ == "__main__":
     d = DummyAgent()
