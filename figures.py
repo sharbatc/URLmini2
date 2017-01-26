6ee631e4bf0af140e609d7a414a21567982bbd88
András Ecker <andras.ecker@epfl.ch>
2017-01-26 22:27:55 +0100

    some minor changes

--- figures.py
+++ figures.py
@@ -18,18 +18,18 @@ for key, val in mplPars.items():
     plt.rcParams[key] = val
 
 
-def plot_escape_time(esc_ts, tau, lambda_, x_size, xdot_size, w_ini):
+def plot_escape_time(esc_ts, tau, lambda_, x_size, xd_size, w_ini):
     """Plots escape time"""
     
     fig = plt.figure(figsize=(10,8))
     ax = fig.add_subplot(1,1,1)
     
     ax.plot(esc_ts, linewidth=2)
-    ax.set_title("Escape time (tau:%.3f, lambda:%.3f, x_size:%s, xdot_size:%s, w_ini:%s)"%(tau, lambda_, x_size, xdot_size, w_ini))
+    ax.set_title("Escape time (tau:%.5f, lambda:%.2f, x_size:%s, xd_size:%s, w_ini:%s)"%(tau, lambda_, x_size, xd_size, w_ini))
     ax.set_xlabel("#{episodes}")
     ax.set_ylabel("Escape time: #{iteration}")
        
-    figName = "figures/esc_time__tau%.3f_lambda%.3f_x_size%s_xdot_size%s_wini%s.jpg"%(tau, lambda_, x_size, xdot_size, w_ini)
+    figName = "figures/esc_time__tau%.5f_lambda%.2f_x_size%s_xd_size%s_wini%s.jpg"%(tau, lambda_, x_size, xd_size, w_ini)
     fig.savefig(figName)
     print("figure saved to:", figName)
     plt.close()
